d374b686b43e313a8fc590cd579df2f0f63db5ed
halpi17 <nishiyamaymyt@gmail.com>
2021-07-25 00:20:57 +0900

    Add: DP

--- BeginnerContest211/C.py
+++ BeginnerContest211/C.py
@@ -2,15 +2,22 @@ class Solution():
     def chokudai(self, S):
         t = 'chokudai'
 
-        dp = [0] * 9
-        dp[0] = 1
+        dp = [[0] * (len(S) + 1) for _ in range(9)]
 
-        for i in range(len(S)):
-            for j in range(8):
-                if (S[i] == t[j]):
-                    dp[j+1] += dp[j]
+        for i in range(9):
+            for j in range(len(S) + 1):
+                if i == 0:
+                    dp[i][j] = 1
+                    continue
+                if j == 0:
+                    dp[i][j] = 0
+                    continue
+                if S[j-1] != t[i-1]:
+                    dp[i][j] = dp[i][j-1]
+                else:
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j-1]
 
-        return dp[8] % (10**9 + 7)
+        return dp[-1][-1] % (10**9 + 7)
 
 def main():
     solution = Solution()
